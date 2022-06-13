de47419ec14d8a8ceccf4e877c3cda5799c03927
Beck049 <beckxiao2001323@gmail.com>
2022-06-14 02:13:59 +0800

    root.php-all function done.

--- root.php
+++ root.php
@@ -23,7 +23,11 @@ session_start();
 			$modify_id = $_POST['modify_id'];
 			$pos_num = 0;
 
-			if( isset($member) )
+			if( isset($_POST['member'])   ) { $pos_num += 1; }
+			if( isset($_POST['driver'])   ) { $pos_num += 2; }
+			if( isset($_POST['analyzer']) ) { $pos_num += 4; }
+			if( isset($_POST['employee']) ) { $pos_num += 8; }
+			if( isset($_POST['root'])     ) { $pos_num += 16;}
 
 			$query = "update person set position = '$pos_num' where person.id = '$modify_id';";
 			mysqli_query($con, $query);
@@ -36,13 +40,13 @@ session_start();
 			<input id="text" type="text" name="modify_id" placeholder="Enter the user ID"><br><br>
 			<input id="checkbox" type="checkbox" name="member" value="member">
 			<label for="member"> member </label>
-			<input id="checkbox" type="checkbox" name="member" value="member">
+			<input id="checkbox" type="checkbox" name="driver" value="driver">
 			<label for="member"> driver </label>
-			<input id="checkbox" type="checkbox" name="member" value="member">
+			<input id="checkbox" type="checkbox" name="analyzer" value="analyzer">
 			<label for="member"> analyzer </label>
-			<input id="checkbox" type="checkbox" name="member" value="member">
+			<input id="checkbox" type="checkbox" name="employee" value="employee">
 			<label for="member"> employee </label>
-			<input id="checkbox" type="checkbox" name="member" value="member">
+			<input id="checkbox" type="checkbox" name="root" value="root">
 			<label for="member"> root </label>
 			<br><br>
 			<input id="button" type="submit" name="ModPosition" value="submit">
