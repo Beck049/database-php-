8a99f43f71e589ecdb236f7bf3f763ab92b4f347
Beck049 <beckxiao2001323@gmail.com>
2022-06-14 07:37:37 +0800

    feat(modify.php): mod amount & del order if 0 elem

--- modify.php
+++ modify.php
@@ -13,10 +13,75 @@ session_start();
 <head>
 	<title>My database project</title>
 	<link rel="stylesheet" href="tabs.css">
+    <link rel="stylesheet" href="float.css">
 </head>
 <body>
-    <!-- search Order
-	         show seller data | buyer data | products -->
-		<!-- select products to delete or delete all Order 
-	         ( check if state is prepareing )  -->
+    <!-- search Order show seller data | buyer data | products -->
+	<!-- select products to delete or delete all Order ( check if state is prepareing )  -->
+    <?php
+    if($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['DelProduct'])) {
+        $order_id = $_POST['order_id'];
+        $p_name = $_POST['del_p_name'];
+        $p_price = $_POST['del_p_price'];
+        $amount = $_POST['mod_amount'];
+
+        // get count
+        $query = "select count(*) as total from contain where order_id = '$order_id'";
+        $result = mysqli_query($con, $query);
+        $count = mysqli_fetch_assoc($result);
+        $count = $count['total'];
+
+        echo $count;
+
+        // get product_id
+        $query = "select product_id from product where p_name = '$p_name' and cost = '$p_price';";
+        $result = mysqli_query($con, $query);
+        $product_id = mysqli_fetch_assoc($result);
+        $product_id = $product_id['product_id'];
+
+        if( $count > 1 ) {
+            // delete contain
+            if($amount == 0) {
+                $query = "delete from contain where contain.order_id = '$order_id' and contain.product_id = '$product_id';";
+			    mysqli_query($con, $query);
+            }
+            // mod
+            else {
+                $query = "update contain set amount = '$amount' where contain.order_id = '$order_id' and contain.product_id = '$product_id';";
+			    mysqli_query($con, $query);
+            }
+        }
+        else if ( $count == 1 ) {
+            // delete contain & order
+            if($amount == 0) {
+                $query = "delete from contain where contain.order_id = '$order_id' and contain.product_id = '$product_id';";
+			    mysqli_query($con, $query);
+
+                $query = "delete from orders where order.order_id = '$order_id';";
+			    mysqli_query($con, $query);
+            }
+            // mod
+            else {
+                $query = "update contain set amount = '$amount' where contain.order_id = '$order_id' and contain.product_id = '$product_id';";
+			    mysqli_query($con, $query);
+            }
+        }
+        else {
+            echo '<p style="color:red;"> error </p>';
+        }
+
+        header("Refresh:0");
+        die;
+    }
+    ?>
+    <div>
+        <h3>Modify Order</h3>
+        <form method="post">
+			<input id="text" type="text" name="order_id" placeholder="Enter the seller ID"><br><br>
+			<input id="text" type="text" name="del_p_name"  placeholder="Enter the product name"><br><br>
+			<input id="text" type="text" name="del_p_price" placeholder="Enter the product price"><br><br>
+			<input id="text" type="text" name="mod_amount" placeholder="Enter the ideal amount"><br><br>
+			<input id="button" type="submit" name="DelProduct" value="Submit">
+		</form>
+    </div>
 </body>
