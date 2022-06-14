1a399ab98c19c9cffebca09c330963a49b0c565d
Yunyun0404 <v04040507@gmail.com>
2022-06-14 09:33:57 +0800

    feat(status): great

--- status.php
+++ status.php
@@ -17,4 +17,64 @@ session_start();
 <body>
     <!-- serach order, show staus 
 	         check status add status -->
+	<div id="box">
+		<form method="post">
+			<input id="text" type="text" name="search_id" placeholder="Enter the order ID">
+			<input id="button" type="submit" name="Search" value="Search"><br>
+		</form>
+	</div>
+	<div style = "padding: 16px; background-color: #ccc;">
+		<?php
+		if($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['Search'])) {
+			$search_id = $_POST['search_id'];
+			
+			$query  = "select count(*) as cnt from status where status.order_id='$search_id'";
+			$result = mysqli_query($con, $query);
+			$data   = mysqli_fetch_assoc($result);
+			$data   = $data['cnt'];
+			
+			if( $data==0 ){
+				$cur_state = 1;
+				$query  = "select id as sell_id from sell where sell.order_id='$search_id'";
+				$result = mysqli_query($con,$query);
+				$seller_id  = mysqli_fetch_assoc($result);
+				$seller_id  = $seller_id['sell_id'];
+				$query  = "select addr as address from person where person.id='$seller_id'";
+				$result = mysqli_query($con,$query);
+				$seller_addr = mysqli_fetch_assoc($result);
+				$seller_addr = $seller_addr[ 'address' ];
+				$address = $seller_addr;
+			}else if( $data==1 ){
+				$cur_state = 2;
+				$query  = "select id as buy_id from buy where buy.order_id='$search_id'";
+				$result = mysqli_query($con,$query);
+				$buyer_id  = mysqli_fetch_assoc($result);
+				$buyer_id  = $buyer_id['buy_id'];
+				$query  = "select addr as address from person where person.id='$buyer_id'";
+				$result = mysqli_query($con,$query);
+				$buyer_addr = mysqli_fetch_assoc($result);
+				$buyer_addr = $buyer_addr[ 'address' ];
+				$address = $buyer_addr;
+			}else if( $data==2 ) echo 'error message';
+
+
+			$id=random_num(20);
+			$query = "insert into trans (id,cur_state,addr) values ('$id','$cur_state','$address')";
+			mysqli_query($con, $query);
+			$query = "select trans_id from trans where trans.id='$id'";
+			$result= mysqli_query($con, $query);
+			$result = mysqli_fetch_assoc($result);
+			$trans_id = $result['trans_id'];
+			$query = "insert into states(order_id,trans_id) values('$search_id','$trans_id')";
+			mysqli_query($con,$query);
+
+			$id=$user_data['id'];
+			$query = "insert into work(id,trans_id) values('$id','$trans_id')";
+			mysqli_query($con,$query);
+			
+			header("Refresh:0");
+			die;
+		}
+		?>
+	</div>
 </body>
