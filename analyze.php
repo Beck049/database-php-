ea8024a2fea425e7e79f9d6549b2da64abbe8fce
Yunyun0404 <v04040507@gmail.com>
2022-06-16 05:48:46 +0800

    目前仍然有bug...QQ

---
+++ analyze.php
@@ -0,0 +1,86 @@
+<?php 
+session_start();
+
+	include("connection.php");
+	include("functions.php");
+
+	$user_data = check_login($con); 
+
+?> 
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My database project</title>
+	<link rel="stylesheet" href="tabs.css">
+</head>
+<body>
+<!-- input: Order_ID 
+	    	output:  buyer | state | products | cost | amount | seller | time -->
+	<div style = "padding: 10px; background-color: #FAEBD7;">
+		<h3>Search by ID</h3>
+            <form method="post">
+            <input id="text" type="text" name="search_id" placeholder="Enter the search ID"><br>
+			<input id="button" type="submit" name="Sell" value="Sell" ><br><br>
+            <input id="button" type="submit" name="Buy" value="Buy"><br>
+		</form>
+	</div>
+	<div style = "padding: 16px; background-color: #FFF8D7;">
+		<?php
+		    if( $_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['Sell']) ) {
+                $search_id = $_POST['search_id'];
+
+                //find order_id
+                $query  = "select order_id from sell where sell.id='$search_id'";
+				$result = mysqli_query($con,$query);
+                if( mysqli_num_rows($result) >0 ){
+                    while( $row = $result->fetch_assoc() ){
+                        $order_id = $row['order_id'];
+                        echo "order_id:".$order_id;
+                        //find product_id
+                        $query  = "select product_id from contain where contain.order_id='$order_id'";
+				        $result = mysqli_query($con, $query);
+				        $data   = mysqli_fetch_assoc($result);
+				        $data   = $data['product_id'];
+                        //find product detail
+                        $query  = "select p_name cost from product where product.product_id='$data'";
+				        $result = mysqli_query($con, $query);
+				        $data   = mysqli_fetch_assoc($result);
+                        echo "name: ". $data['p_name']." - cost: ".$data['cost']."<br>";                    
+                    }
+                }else {
+                    echo '0 results';
+                }
+		    }
+		?>
+    </div>
+	<div style = "padding: 16px; background-color: #FFF8D7;">
+        <?php
+            if( $_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['Buy']) ) {
+                $search_id = $_POST['search_id'];
+
+                //find order_id
+                $query  = "select order_id from buy where buy.id='$search_id'";
+				$result = mysqli_query($con,$query);
+                if( mysqli_num_rows($result) >0 ){
+                    while( $row = $result->fetch_assoc() ){
+                        $order_id = $row['order_id'];
+                        echo "order_id:".$order_id;
+                        //find product_id
+                        $query  = "select product_id from contain where contain.order_id='$order_id'";
+				        $result = mysqli_query($con, $query);
+				        $data   = mysqli_fetch_assoc($result);
+				        $data   = $data['product_id'];
+                        //find product detail
+                        $query  = "select p_name cost from product where product.product_id='$data'";
+				        $result = mysqli_query($con, $query);
+				        $data   = mysqli_fetch_assoc($result);
+                        echo "name: ". $data['p_name']." - cost: ".$data['cost']."<br>";                    
+                    }
+                }else {
+                    echo '0 results';
+                }
+		    }
+        ?>
+    </div>
+</body>
